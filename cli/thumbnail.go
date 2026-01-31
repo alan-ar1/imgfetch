db3b8dd428be775d46d5ce973bfb06070aa95223
alan-ar1 <alan123qwer@gmail.com>
2026-02-01 00:33:10 +0300

    perf: improve ffmpeg command

--- cli/thumbnail.go
+++ cli/thumbnail.go
@@ -8,7 +8,7 @@ import (
 )
 
 func getVideoThumbnail(videoPath string) (image.Image, error) {
-	cmd := exec.Command("ffmpeg", "-i", videoPath, "-ss", "00:00:05", "-vframes", "1", "-f", "image2pipe", "-vcodec", "mjpeg", "pipe:1")
+	cmd := exec.Command("ffmpeg", "-ss", "00:00:00.001", "-i", videoPath, "-vframes", "1", "-f", "image2pipe", "-vcodec", "mjpeg", "pipe:1")
 
 	var stdout bytes.Buffer
 	cmd.Stdout = &stdout
